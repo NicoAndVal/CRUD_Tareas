6a3b88d105d4d1e92fa941ac4cb3b20be6e659f4
nicolas andres Andrade <nico123andval@gmail.com>
2020-04-19 08:09:27 -0400

    se agrega composer

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+   require('vendor/autoload.php');
+
    require_once 'controlador/plantilla.controlador.php';
    require_once 'modelos/formulario.modelo.php';
    require_once 'controlador/formulario.controlador.php';
