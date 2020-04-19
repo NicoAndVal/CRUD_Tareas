fb024f24f69890397d234a6e43d1821e53f38936
nicolas andres Andrade <nico123andval@gmail.com>
2020-04-19 08:59:34 -0400

    se intenta conectar a al base de datos

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,4 +1,14 @@
 <?php
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+
    Class Conexion{
        static public function conectar(){
            $link = new PDO("mysql:host=localhost;dbname=crud_tareas","root","");
@@ -8,3 +18,4 @@
        }
    }
 ?>
+
